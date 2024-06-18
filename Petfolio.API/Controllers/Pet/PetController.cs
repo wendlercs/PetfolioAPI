8a24055a3da62018e5770d477d7b58d530593729
Wendler Custódio <wendlermoa@gmail.com>
2024-06-17 21:03:58 -0300

    Base project

---
+++ Petfolio.API/Controllers/Pet/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pet.Update;
+using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.Controllers.Pet;
+
+public class PetController : PetfolioApiBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredPetJson), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestPetJson request)
+    {
+        var response = new RegisterPetUseCase().Execute(request);
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
+    {
+        var useCase = new UpdatePetUseCase();
+
+        useCase.Execute(id, request);
+
+        return NoContent();
+    }
+}
---
+++ Petfolio.API/Controllers/PetfolioApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Petfolio.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PetfolioApiBaseController : ControllerBase
+{
+
+}
